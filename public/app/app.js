163fece16630e622b1db5cda16f0bd91302661ce
Dylan <dyl_654@yahoo.com>
2016-08-09 18:01:59 -0400

    added to recipe tab and put login page into separate html file

--- public/app/app.js
+++ public/app/app.js
@@ -9,3 +9,27 @@ ShoppingListApp.controller('TabController',function(){
         return this.tab === givenTab;
     };
 });
+ShoppingListApp.controller('RecipeController', function(){
+    this.recipe = [
+        {
+            name: "Smores",
+            ingredients: "4 graham crackers, 1 chocolate bar, 4 marshmallows",
+            directions: "1. Roast marshmallows. 2. Put chocolate and marshmallows on graham crackers. 3. Enjoy!",
+            addToMenu: false
+        },
+        {
+            name: "Cereal",
+            ingredients: "1 box of cereal, 1c of milk",
+            directions: "Combine cereal and milk",
+            addToMenu: false
+        }
+    ];
+    this.changeAddToMenu = function(){
+        if(this.recipe.addToMenu === true)
+        {
+            this.recipe.addToMenu = false;
+        }else{
+            this.recipe.addToMenu = true;
+        }
+    };
+});
