a2065b60cb4267fdc6c15e5478f6438f97679357
Dylan <dyl_654@yahoo.com>
2016-08-10 00:49:19 -0400

    started making directives

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import cherrypy
+ingredients = ["cheese"]
 
 class HelloWorld(object):
     @cherrypy.expose
+    @cherrypy.tools.json_out()
     def index(self):
-        return "Hello World!"
+        return ingredients
+    @cherrypy.expose
+    def add(self, i):
+        ingredients.append(i)
 
 if __name__ == '__main__':
     cherrypy.tree.mount(None, '/', "public.conf")
--- public/app/app.js
+++ public/app/app.js
@@ -1,21 +1,52 @@
 var ShoppingListApp = angular.module('ShoppingListApp',[]);
 
 ShoppingListApp.controller('TabController',function(){
-    this.tab = 1;
-    this.setTab=function(selectedTab){
-      this.tab = selectedTab;  
-    };
-    this.isSet=function(givenTab){
-        return this.tab === givenTab;
-    };
-});
-ShoppingListApp.controller('RecipeController', function(){
-    this.recipe = [
+            this.tab = 1;
+            this.setTab=function(selectedTab){
+                this.tab = selectedTab;  
+            };
+            this.isSet=function(givenTab){
+                return this.tab === givenTab;
+            };
+       });
+
+//ShoppingListApp.directive("recipeDirective", function($scope){
+//   return{
+//        restrict: 'E',
+//        templateUrl: 'recipe-tab.html'
+////        controller: function($scope){
+////            $scope.ingredient = "";
+////            $scope.recipe = [
+////                {
+////                    name: "Smores",
+////                    ingredients: "4 graham crackers, 1 chocolate bar, 4 marshmallows",
+////                    directions: "1. Roast marshmallows. 2. Put chocolate and marshmallows on graham crackers. 3. Enjoy!",
+////                    addToMenu: true
+////                },
+////                {
+////                    name: "Cereal",
+////                    ingredients: "1 box of cereal, 1c of milk",
+////                    directions: "Combine cereal and milk",
+////                    addToMenu: false
+////                }
+////            ];
+////            $scope.addIngredient = function(ingredient){
+////                alert(ingredient);
+////
+////            };
+////        },
+////       controllerAs: recipeDirect 
+//       
+//   }; 
+//});
+ShoppingListApp.controller('recipeController', function($scope){
+    //$scope.ingredient = "";
+    $scope.recipe = [
         {
             name: "Smores",
             ingredients: "4 graham crackers, 1 chocolate bar, 4 marshmallows",
             directions: "1. Roast marshmallows. 2. Put chocolate and marshmallows on graham crackers. 3. Enjoy!",
-            addToMenu: false
+            addToMenu: true
         },
         {
             name: "Cereal",
@@ -24,12 +55,8 @@ ShoppingListApp.controller('RecipeController', function(){
             addToMenu: false
         }
     ];
-    this.changeAddToMenu = function(){
-        if(this.recipe.addToMenu === true)
-        {
-            this.recipe.addToMenu = false;
-        }else{
-            this.recipe.addToMenu = true;
-        }
+    $scope.addIngredient = function(ingredient){
+        alert(ingredient);
+
     };
 });
